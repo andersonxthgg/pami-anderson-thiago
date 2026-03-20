a38a64f4a793d4aa3bdd06e0dd4a4f1f967c1753
thiago <thiagoanderson707@gmail.com>
2026-03-19 22:23:24 -0300

    update

--- Aula01/Basico.js
+++ Aula01/Basico.js
@@ -2,3 +2,17 @@
 var VariavelVar = "Olá";
 let VariavelLet = 'Tudo bem?';
 const VariavelConst = 11;
+
+console.log("Olá mundo!")
+console.log(VariavelVar)
+console.log(VariavelVar, VariavelLet)
+console.log(VariavelConst + VariavelConst)//concatenação
+
+
+VariavelLet = 4
+console.log(VariavelLet)
+
+//VariavelConst = 6
+//A regra é que a constante não pode 
+//ser reatribuida
+console.log(VariavelConst)
