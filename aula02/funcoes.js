9e36d00ec3b844cf339e70468a2e91793730b268
thiago <thiagoanderson707@gmail.com>
2026-03-26 22:09:39 -0300

    update

---
+++ aula02/funcoes.js
@@ -0,0 +1,26 @@
+// kebab Case é um padrão de escrita que separa as 
+// palavras com - , podendo também ser SCREAMING
+
+function SomaNumeros () {
+    let valor_i = 6
+    let valor_ii = 5
+    let valor_resultado = 0
+
+    return resultado = valor_i + valor_ii
+}
+
+console.log(SomaNumeros())
+
+function SomaNumnerosII (valor_i, valor_ii) {
+    console.log(valor_i + valor_ii)
+}
+
+SomaNumnerosII(9, 15)
+SomaNumnerosII(3, 4)
+SomaNumnerosII(90, 50)
+
+const SOMA_NUMEROS_III = (valor_i, valor_ii) => {
+    return valor_i + valor_ii
+}
+
+console.log(SOMA_NUMEROS_III(81, 98))
---
+++ aula02/numero.js
@@ -0,0 +1,33 @@
+//sanake case é caracterizada por utilizar o underline/undescore(_)
+//para separar suas palavras. Pode ser combinado entre letras
+//maiuscuilos (Streaming) ou minusculas
+
+
+let primeiro_valor = 0
+let segundo_valor = 0 
+
+primeiro_valor = 4
+segundo_valor = 5
+
+console.log(
+    '${primeiro_valor} + ${segundo_valor} = ',
+    primeiro_valor + segundo_valor
+)
+
+console.log(
+    '${primeiro_valor} - ${segundo_valor} = ',
+    primeiro_valor - segundo_valor
+)
+
+console.log(
+    '${primeiro_valor} / ${segundo_valor} = ',
+    primeiro_valor / segundo_valor
+)
+
+console.log(
+    '${primeiro_valor} * ${segundo_valor} = ',
+    primeiro_valor * segundo_valor
+)
+
+let variavel
+console.log(typeof(variavel))
